f123291d5b84559bbc5389c28595e26ba5d739aa
vitoeko <vitoeko01@gmail.com>
2019-09-15 13:21:21 +0700

    jadwal

---
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.note
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab!!.setOnClickListener {pindah()}
+    }
+
+    fun pindah(){
+        val i=Intent(this,coba_activity::class.java)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/note/coba_activity.kt
@@ -0,0 +1,12 @@
+package com.example.note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class coba_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coba_activity)
+    }
+}
